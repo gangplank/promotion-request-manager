5b2cb325d838b363b2a0791b62b863079ed0e691
Codefist <codefist0@gmail.com>
2016-02-25 04:16:31 +0000

    Implement barebones /login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ActionController::Base
-  def create
+  authem_for :user
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      sign_in(user)
+      redirect_back_or_to(root_path)
+    else
+      flash.now.alert = "Your email and password do not match"
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out_user
+    redirect_to :root
   end
 end
