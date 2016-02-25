ee2994532ebfac11ce4c1fa6520b2ab6093a3874
David Monaghan <github@allstruck.com>
2016-02-25 08:51:31 -0700

    Cleaning up index output.

--- app/models/promo_request.rb
+++ app/models/promo_request.rb
@@ -2,4 +2,5 @@ class PromoRequest < ActiveRecord::Base
   has_many :promo_channel_promo_requests
   has_many :promo_channels, through: :promo_channel_promo_requests
   belongs_to :gangplank_location
+  belongs_to :gangplank_initiative
 end
