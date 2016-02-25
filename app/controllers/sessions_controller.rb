bf0a4fa13a3b1a5c125a0e52c19af25dcf1a99a1
Codefist <codefist0@gmail.com>
2016-02-25 02:25:44 +0000

    Add authem and haml

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,5 @@
+class SessionsController < ActionController::Base
+  def create
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225010938) do
+ActiveRecord::Schema.define(version: 20160225021311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authem_sessions", force: :cascade do |t|
+    t.string   "role",                    null: false
+    t.integer  "subject_id",              null: false
+    t.string   "subject_type",            null: false
+    t.string   "token",        limit: 60, null: false
+    t.datetime "expires_at",              null: false
+    t.integer  "ttl",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "authem_sessions", ["expires_at", "subject_type", "subject_id"], name: "index_authem_sessions_subject", using: :btree
+  add_index "authem_sessions", ["expires_at", "token"], name: "index_authem_sessions_on_expires_at_and_token", unique: true, using: :btree
 
   create_table "gangplank_initiatives", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +62,14 @@ ActiveRecord::Schema.define(version: 20160225010938) do
     t.string   "press_release_uri"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                           null: false
+    t.string   "password_digest",                 null: false
+    t.string   "password_reset_token", limit: 60, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
